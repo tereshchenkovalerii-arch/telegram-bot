b404c8d4a4f7a749f5596df354238cfc2a2798db
tereshchenkovalerii-arch <tereshchenko.valerii@gmail.com>
2026-03-10 21:23:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,22 +6,48 @@ TOKEN = os.environ.get("BOT_TOKEN")
 GROUP_ID = int(os.environ.get("GROUP_ID"))
 
 async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    # Игнорируем сообщения из группы
+    if update.effective_chat.id == GROUP_ID:
+        return
+
     user = update.message.from_user
 
-    text = f"""
+    header = f"""
 Новое сообщение
 
 Имя: {user.first_name}
-Username: @{user.username}
-
-Сообщение:
-{update.message.text}
+Username: @{user.username if user.username else 'нет'}
 """
 
-    await context.bot.send_message(chat_id=GROUP_ID, text=text)
+    # Текст
+    if update.message.text:
+        text = f"{header}\nСообщение:\n{update.message.text}"
+        await context.bot.send_message(chat_id=GROUP_ID, text=text)
+
+    # Фото
+    elif update.message.photo:
+        photo = update.message.photo[-1].file_id
+        await context.bot.send_photo(chat_id=GROUP_ID, photo=photo, caption=header)
+
+    # Документы
+    elif update.message.document:
+        file_id = update.message.document.file_id
+        await context.bot.send_document(chat_id=GROUP_ID, document=file_id, caption=header)
+
+    # Видео
+    elif update.message.video:
+        file_id = update.message.video.file_id
+        await context.bot.send_video(chat_id=GROUP_ID, video=file_id, caption=header)
+
+    # Аудио
+    elif update.message.audio:
+        file_id = update.message.audio.file_id
+        await context.bot.send_audio(chat_id=GROUP_ID, audio=file_id, caption=header)
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT, forward))
+app.add_handler(MessageHandler(filters.ALL, forward))
 
 app.run_polling()
